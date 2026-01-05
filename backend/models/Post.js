8cef2c3917e0b686afda1714790f2327c915b5bc
abhinav3204l <245122748014@mvsrec.edu.in>
2026-01-06 00:38:09 +0530

    fix: posts api + remove localhost usage

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema(
   {
@@ -8,7 +8,7 @@ const PostSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      required: true,
+      default: "farmer",
     },
     likes: {
       type: Number,
@@ -18,5 +18,4 @@ const PostSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Post", PostSchema);
-// Collection name will be: posts
+module.exports = mongoose.model("Post", PostSchema);
