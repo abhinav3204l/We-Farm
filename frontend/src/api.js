e3bf671911206464cb534b6f9d82cf0c85357b39
abhinav3204l <245122748014@mvsrec.edu.in>
2026-04-29 10:39:17 +0530

    update api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-service-name.onrender.com",
+  baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
